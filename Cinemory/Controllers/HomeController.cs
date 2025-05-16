966e9340460d3e5cbbc216a2bdc1f16f5e6ad3f9
soner keserci <sonerkeserci@gmail.com>
2025-05-16 03:46:41 +0300

    Adding HomeController and LandingView

--- Cinemory/Controllers/HomeController.cs
+++ Cinemory/Controllers/HomeController.cs
@@ -9,12 +9,32 @@ namespace Cinemory.Controllers
     {
         private readonly UserManager<AppUser> _userManager;
 
-        public HomeController(UserManager<AppUser> userManager)
+
+        public HomeController(UserManager<AppUser> userManager)    //dependency injection ile UserManager servisi
         {
             _userManager = userManager;
         }
 
-        public IActionResult Index()
+
+        public async Task<IActionResult> Index()  // asenkron... bu siteye ilk girildiğinde çalışır
+        {
+
+            if (User?.Identity?.IsAuthenticated != true) // giriş yapmamışsa "Landing.cshtml" view'ını döndür
+                return View("Landing");
+
+
+            var user = await _userManager.GetUserAsync(User); // yapmışsa, UserManager üzerinden login olan kullanıcıyı getir
+
+
+            if (user != null && await _userManager.IsInRoleAsync(user, "Admin")) // kullanıcı "Admin" rolündeyse Dashboard
+                return RedirectToAction("Index", "Dashboard");
+
+
+            return RedirectToAction("ActivityFeed", "UserFeed"); // Admin değilse, normal kullanıcı feed sayfası
+        }
+
+
+        public IActionResult Landing()
         {
             return View();
         }
--- Cinemory/Program.cs
+++ Cinemory/Program.cs
@@ -35,6 +35,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Dashboard}/{action=Index}/{id?}");
+    pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
