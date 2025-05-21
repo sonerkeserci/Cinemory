fa7e736934ff3a9f67e5829f0944fd0d78be5842
soner keserci <sonerkeserci@gmail.com>
2025-05-21 06:18:12 +0300

    MovieInteractionModel

---
+++ Cinemory/Models/ViewModels/MovieInteractionViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Cinemory.Models.ViewModels
+{
+    public class MovieInteractionViewModel
+    {
+        public int MovieId { get; set; }
+        public bool IsFavorite { get; set; }
+        public bool IsInWatchlist { get; set; }
+        public int? Rating { get; set; }
+    }
+}
