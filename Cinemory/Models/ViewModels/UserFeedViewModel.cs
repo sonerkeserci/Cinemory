2ac82757232e1d785ffbcb1f7e270c145847178d
soner keserci <sonerkeserci@gmail.com>
2025-05-23 11:05:24 +0300

    Adding UserFeedViewModel.

---
+++ Cinemory/Models/ViewModels/UserFeedViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Cinemory.Models.ViewModels
+{
+    public class UserFeedViewModel
+    {
+        public List<MovieInteractionViewModel> LastWatched { get; set; } = new();
+        public List<MovieInteractionViewModel> Watchlisted { get; set; } = new();
+        public List<MovieInteractionViewModel> RecentlyAdded { get; set; } = new();
+        public List<MovieInteractionViewModel> Recommended { get; set; } = new();
+        public string? RssWidgetUrl { get; set; }
+    }
+}
